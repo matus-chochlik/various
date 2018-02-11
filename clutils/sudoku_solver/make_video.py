317fc6484f6ea09c35af7487a9ff8b4d6475564f
Matus Chochlik <chochlik@gmail.com>
2018-02-11 09:29:56 +0100

    Minor updates in the sudoku solver make video script

--- clutils/sudoku_solver/make_video.py
+++ clutils/sudoku_solver/make_video.py
@@ -5,10 +5,13 @@ from __future__ import print_function
 
 import os
 import cv2
+import numpy
 import subprocess
 import fileinput
-import tempfile
 import functools
+import collections
+import threading
+import multiprocessing
 
 def read_frames():
 	table = unicode()
@@ -34,64 +37,78 @@ def make_asciiart():
 				"frame": frame
 			}, width*10, height*17
 
-def render_images():
-	for asciiart, width, height in make_asciiart():
-		with tempfile.NamedTemporaryFile('w', delete=False) as art_file:
-			art_file.write(asciiart.encode('utf-8'))
+class RenderThread(threading.Thread):
+	def __init__(self, renderer, art_input):
+		self._renderer = renderer
+		self._input = art_input
+		threading.Thread.__init__(self,target=self._run)
+		threading.Thread.start(self)
 
+	def _run(self):
+		self._value = bytearray(self._renderer.communicate(self._input)[0])
+
+	def get(self):
+		threading.Thread.join(self)
+		return self._value
+
+def render_images():
+	for art_input, width, height in make_asciiart():
 		conv_cmd = [
 			"convert",
 			"-size", str(width)+"x"+str(height), "xc:white",
 			"-font", "DejaVu-Sans-Mono", "-pointsize", "14",
 			"-fill", "black",
-			"-draw", "@"+art_file.name, art_file.name+".png"
+			"-draw", "@-", "PNG8:-"
 		]
-		yield subprocess.Popen(conv_cmd), art_file.name, art_file.name+".png"
+		renderer = subprocess.Popen(
+			conv_cmd,
+			stdin=subprocess.PIPE,
+			stdout=subprocess.PIPE
+		)
+
+		yield RenderThread(renderer, art_input.encode('utf-8'))
+
+def load_image(future_img_data):
+	return cv2.imdecode(
+		numpy.asarray(future_img_data.get(), dtype=numpy.uint8),
+		cv2.IMREAD_UNCHANGED
+	)
 
 def load_images():
-	queue = list()
-	backlog = 8
-	for _renderer, _art_path, _img_path in render_images():
+	queue = collections.deque()
+	backlog = multiprocessing.cpu_count()*2
+	for renderer in render_images():
 		if len(queue) < backlog:
-			queue.append((_renderer, _art_path, _img_path))
+			queue.append(renderer)
 		if len(queue) >= backlog:
-			renderer, art_path, img_path = queue[0]
-			queue = queue[1:]
-			renderer.wait()
-			yield cv2.imread(img_path)
-			os.remove(art_path)
-			os.remove(img_path)
+			yield load_image(queue.popleft())
 
-	for renderer, art_path, img_path in queue:
-		renderer.wait()
-		yield cv2.imread(img_path)
-		os.remove(art_path)
-		os.remove(img_path)
+	for renderer in queue:
+		yield load_image(renderer)
 
 
 def blend_images(imgs):
-	return functools.reduce(lambda x, y: cv2.addWeighted(x, 0.5, y, 0.5, 0), imgs)
+	return functools.reduce(lambda x, y: cv2.addWeighted(x, 0.6, y, 0.4, 0), imgs)
 
-def blur_images():
+def blur_images(count, step):
 	group = list()
-	n = 4;
-	m = 1;
 	for image in load_images():
-		if len(group) < n:
+		if len(group) < count:
 			group.append(image);
 
-		if len(group) >= n:
+		if len(group) >= count:
 			yield blend_images(group)
-			group = group[m:]
+			group = group[step:]
 
 	while len(group) > 0:
 		yield blend_images(group)
-		group = group[m:]
+		group = group[step:]
+	yield image
 
 def make_video(out_path, fps = 60, size = None, video_format = "DIV4"):
 	fourcc = cv2.VideoWriter_fourcc(*video_format)
 	video = None
-	for image in blur_images():
+	for image in blur_images(count=16, step=8):
 		if image is not None:
 			if size is None:
 				size = image.shape[1], image.shape[0]
