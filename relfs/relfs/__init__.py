9c86b005e33f7473350618e247aad1a09f8414a1
Matus Chochlik <chochlik@gmail.com>
2019-04-28 09:54:35 +0200

    Removed the old pgsql database from relfs

--- relfs/relfs/__init__.py
+++ relfs/relfs/__init__.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 #------------------------------------------------------------------------------#
 from .core import Repository, open_repository
-from .database import Database , open_database
 from .arguments import ArgumentSetup, make_argument_parser
 from .error import RelFsError, print_error
 #------------------------------------------------------------------------------#
