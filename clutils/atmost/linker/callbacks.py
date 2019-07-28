076ba1401c2583b67784f04aa1b3caf7578ab455
Matus Chochlik <chochlik@gmail.com>
2019-07-28 10:27:48 +0200

    Updated the atmost linker callback script + train ML model script

---
+++ clutils/atmost/linker/callbacks.py
@@ -0,0 +1,168 @@
+# coding: UTF-8
+#  Copyright (c) 2019 Matus Chochlik
+import os
+import sys
+import json
+import multiprocessing
+# ------------------------------------------------------------------------------
+class DriverData(object):
+    # --------------------------------------------------------------------------
+    def __init__(self):
+        pass
+# ------------------------------------------------------------------------------
+def load_user_data():
+    sys.stdout.write("[{}\n")
+
+# ------------------------------------------------------------------------------
+def save_user_data(user_data):
+    sys.stdout.write("]")
+
+# ------------------------------------------------------------------------------
+def is_linker(proc):
+    linker_names = [
+        'x86_64-linux-gnu-gold',
+        'x86_64-linux-gnu-ld',
+        'gold',
+        'ld'
+    ]
+    return proc.basename() in linker_names
+
+# ------------------------------------------------------------------------------
+def get_ld_info(user_data, proc):
+    assert is_linker(proc)
+    args = proc.args()[1:]
+    cwd = proc.cwd()
+    outputs = []
+    sinputs = []
+    dinputs = []
+    plugins = []
+    libdirs = []
+
+    prev = None
+    for arg in args:
+        if prev in ["-L", "--library-path"]:
+            if os.path.isdir(arg):
+                libdirs.append(arg)
+        elif arg.startswith("-L"):
+            tmp = arg[len("-L"):].strip()
+            if os.path.isdir(tmp):
+                libdirs.append(tmp)
+        elif arg.startswith("--library-path"):
+            tmp = arg[len("--library-path"):].strip()
+            if os.path.isdir(tmp):
+                libdirs.append(tmp)
+        prev = arg
+
+    for arg in ["/usr/local/lib", "/usr/lib", "/lib"]:
+        if os.path.isdir(arg):
+            libdirs.append(arg)
+
+    libdirs = set([os.path.realpath(p) for p in libdirs])
+
+    def _is_so_path(path):
+        temp = path
+        while temp:
+            temp, ext = os.path.splitext(temp)
+            if not ext:
+                break
+            if ext == ".so":
+                return True
+        return False
+
+    def _append_lib(name):
+        for libdir in libdirs:
+            if os.path.isfile(os.path.join(libdir,  "lib%s.so" % name)):
+                dinputs.append(os.path.join(libdir, "lib%s.so" % name))
+                break
+            elif os.path.isfile(os.path.join(libdir, "lib%s.a" % name)):
+                sinputs.append(os.path.join(libdir,  "lib%s.a" % name))
+                break
+
+    def _append_plgn(path):
+        if os.path.isfile(path):
+            plugins.append(path)
+        elif os.path.isfile(os.path.join(cwd, path)):
+            plugins.append(os.path.join(cwd, path))
+
+    prev = None
+    for arg in args:
+        if arg[0] != '-':
+            path_arg = None
+            if os.path.isfile(arg):
+                path_arg = arg
+            elif os.path.isfile(os.path.join(cwd, arg)):
+                path_arg = os.path.join(cwd, arg)
+
+            if path_arg is not None:
+                if prev in ["-o", "--output"]:
+                    outputs.append(path_arg)
+                elif prev in ["--plugin", "-plugin"]:
+                    plugins.append(path_arg)
+                elif _is_so_path(path_arg):
+                    dinputs.append(path_arg)
+                else:
+                    sinputs.append(path_arg)
+        elif prev in ["-l", "--library"]:
+            _append_lib(arg)
+        elif arg.startswith("-l"):
+            _append_lib(arg[len("-l"):].strip())
+        elif arg.startswith("--library"):
+            _append_lib(arg[len("--library"):].strip())
+        elif arg.startswith("--plugin"):
+            _append_plgn(arg[len("--plugin"):].strip())
+        elif arg.startswith("-plugin"):
+            _append_plgn(arg[len("-plugin"):].strip())
+
+        prev = arg
+
+    sinputs = set([os.path.realpath(p) for p in sinputs])
+    dinputs = set([os.path.realpath(p) for p in dinputs])
+    plugins = set([os.path.realpath(p) for p in plugins])
+
+    sco = sum(1 for f in sinputs if os.path.isfile(f))
+    dco = sum(1 for f in dinputs if os.path.isfile(f))
+    npg = sum(1 for f in plugins if os.path.isfile(f))
+    ssz = sum(os.path.getsize(f) for f in sinputs if os.path.isfile(f))
+    dsz = sum(os.path.getsize(f) for f in dinputs if os.path.isfile(f))
+    osz = sum(os.path.getsize(f) for f in outputs if os.path.isfile(f))
+    mem = proc.max_memory_bytes()
+
+    if osz > 0 and len(outputs) > 0:
+        pie = 1 if ("-pie" in args or "--pic-executable" in args) else 0
+        pie = 0 if ("-no-pie" in args or "--no-pic-executable" in args) else pie 
+        outpath = os.path.realpath(outputs[0])
+
+        return {
+            "plugin_count": npg,
+            "static_count": sco,
+            "static_size": ssz,
+            "shared_count": dco,
+            "shared_size": dsz,
+            "memory_size": mem,
+            "output_size": osz,
+            "pie": pie,
+            "b_static": 1 if "-Bstatic" in args else 0,
+            "b_dynamic": 1 if "-Bdynamic" in args else 0,
+            "output_path": outpath
+        }
+
+
+# ------------------------------------------------------------------------------
+def let_process_go(user_data, procs):
+    proc = procs.current()
+    if is_linker(proc):
+        if len(procs.active()) < 1:
+            return True
+        return False
+    return len(procs.active()) <  multiprocessing.cpu_count()
+
+# ------------------------------------------------------------------------------
+def process_finished(user_data, proc):
+    if is_linker(proc):
+        sys.stdout.write(",")
+        json.dump(get_ld_info(user_data, proc), sys.stdout)
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+
+
+ 
---
+++ clutils/atmost/linker/train_model.py
@@ -0,0 +1,196 @@
+# coding: UTF-8
+#  Copyright (c) 2019 Matus Chochlik
+import os
+import sys
+import csv
+import json
+import glob
+import math
+import pandas
+import argparse
+
+# ------------------------------------------------------------------------------
+class TrainModelArgumentParser(argparse.ArgumentParser):
+    # --------------------------------------------------------------------------
+    def _size_bytes(self, x):
+        try:
+            return long(x)
+        except ValueError:
+            self.error("invalid size in bytes '%s'" % x)
+
+    # --------------------------------------------------------------------------
+    def _positive_int(self, x):
+        try:
+            assert(int(x) > 0)
+            return int(x)
+        except:
+            self.error("`%s' is not a positive integer value" % str(x))
+
+    # --------------------------------------------------------------------------
+    def __init__(self, **kw):
+        argparse.ArgumentParser.__init__(self, **kw)
+
+        self.add_argument(
+            "--input", "-i",
+            metavar='INPUT-PATH',
+            nargs='?',
+            dest="input_args",
+            default=list(),
+            action="append"
+        )
+
+        self.add_argument(
+            "arguments",
+            metavar="INPUT-PATH",
+            nargs='*',
+            type=str,
+            help="input JSON files"
+        )
+
+        self.add_argument(
+            "--chunk-size", "-C",
+            nargs='?',
+            dest="chunk_size",
+            type=self._size_bytes,
+            default=1024*1024*1024,
+            action="store"
+        )
+
+        self.add_argument(
+            "--repeat-count", "-R",
+            nargs='?',
+            dest="repeat_count",
+            type=self._positive_int,
+            default=24,
+            action="store"
+        )
+
+    # --------------------------------------------------------------------------
+    def process_parsed_options(self, options):
+        options.input_paths = list()
+
+        _not_file_fmt = "'%s' is not a file path or pattern"
+        for arg in options.input_args + options.arguments:
+            if os.path.isfile(arg):
+                options.input_paths.append(arg)
+            else:
+                for path in glob.glob(arg):
+                    if os.path.isfile(path):
+                        options.input_paths.append(path)
+                    else:
+                        self.error(_not_file_fmt % (path,))
+
+        options.input_paths =\
+            list(set(os.path.realpath(x) for x in options.input_paths))
+
+        del options.__dict__["input_args"]
+        del options.__dict__["arguments"]
+
+        return options
+
+    # --------------------------------------------------------------------------
+    def parse_args(self):
+        return self.process_parsed_options(
+            argparse.ArgumentParser.parse_args(self)
+        )
+
+# ------------------------------------------------------------------------------
+def make_argparser():
+    return TrainModelArgumentParser(
+        prog="train_model",
+        description="""
+        Trains a ML model for the atmost linker callbacks script.
+        """
+    )
+
+# ------------------------------------------------------------------------------
+def get_json_data(options):
+    transforms = [
+        ("pie", lambda x: x),
+        ("static_count", lambda x: x),
+        ("static_size", lambda x: x),
+        ("shared_count", lambda x: x),
+        ("shared_size", lambda x: x),
+        ("memory_size", lambda x: int(math.ceil(float(x) / options.chunk_size)))
+    ]
+    for filepath in options.input_paths:
+        try:
+            with open(filepath, mode="rt") as jsonfile:
+                for row in json.load(jsonfile):
+                    try:
+                        yield dict(
+                            (name, [transf(row[name])]) \
+                            for name, transf in transforms
+                        )
+                    except KeyError: pass
+                    except TypeError: pass
+        except Exception as error:
+            print(error)
+
+# ------------------------------------------------------------------------------
+def train_model(options):
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.neural_network import MLPClassifier
+    from sklearn.metrics import accuracy_score
+
+    model = MLPClassifier()
+    scaler = StandardScaler()
+
+    for d in get_json_data(options):
+        try:
+            df = pandas.DataFrame.from_dict(d)
+            x = df.drop(["memory_size"], axis=1)
+            scaler.partial_fit(x)
+        except: pass
+
+    _1GiB = 1024.0*1024.0*1024.0
+    rng = 64
+    cls = xrange(rng);
+
+    ssum = 0.0
+    scnt = 1.0
+    maxd = 0
+    mind = 0
+
+    for i in xrange(options.repeat_count):
+        print(
+            "run: %5d (%1.3f, %7.3f, %7.3f)" % (
+                i,
+                ssum/scnt,
+                mind*options.chunk_size/_1GiB,
+                maxd*options.chunk_size/_1GiB
+            ))
+        ssum = 0.0
+        scnt = 1.0
+        maxd = -rng
+        mind = +rng
+
+        for d in get_json_data(options):
+            df = pandas.DataFrame.from_dict(d)
+            try:
+                x = df.drop(["memory_size"], axis=1)
+                y = df["memory_size"]
+                x = scaler.transform(x)
+
+                p = 0
+                try:
+                    p = model.predict(x)
+                    ssum += accuracy_score(y, p);
+                    scnt += 1.0
+                except: pass
+
+                diff = p - y[0]
+                maxd = max(maxd, diff)
+                mind = min(mind, diff)
+
+                model.partial_fit(x, y, classes = cls)
+
+            except KeyError as err:
+                print(err)
+# ------------------------------------------------------------------------------
+def main():
+    sys.exit(train_model(make_argparser().parse_args()))
+# ------------------------------------------------------------------------------
+if __name__ == '__main__':
+    main()
+# ------------------------------------------------------------------------------
