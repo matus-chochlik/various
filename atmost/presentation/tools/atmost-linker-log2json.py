59399db7e01b42fb5ebd26e7053e6e8984c5e129
Matus Chochlik <chochlik@gmail.com>
2019-11-04 07:05:24 +0100

    Multiple chnages related to the annotin tool

--- atmost/presentation/tools/atmost-linker-log2json.py
+++ atmost/presentation/tools/atmost-linker-log2json.py
@@ -7,9 +7,9 @@ import json
 import gzip
 
 re_filename = re.compile(".*\.([0-9]+)\.txt.*")
-re_arrived = re.compile("^({.*})\|atmost: arrived '(.+)'")
-re_linking = re.compile("^({.*})\|atmost: linking '(.+)': \(predicted=([0-9.-]+)G±([0-9.-]+)G\|available=([0-9.-]+)G\)")
-re_linked  = re.compile("^({.*})\|atmost: linked  '(.+)': \(predicted=([0-9.-]+)G±([0-9.-]+)G\|actual=([0-9.-]+)G\)")
+re_arrived = re.compile("^atmost: arrived '(.+)'")
+re_linking = re.compile("^atmost: linking '(.+)': \(predicted=([0-9.-]+)G±([0-9.-]+)G\|available=([0-9.-]+)G\)")
+re_linked  = re.compile("^atmost: linked  '(.+)': \(predicted=([0-9.-]+)G±([0-9.-]+)G\|actual=([0-9.-]+)G\)")
 
 try:
     data = []
@@ -23,29 +23,27 @@ try:
                 while True:
                     line = inp.readline()
                     if not(line): break
-                    attr = {}
+                    attr = json.loads(line)
+                    line = attr["line"]
                     match = re_arrived.search(line)
                     if match:
-                        attr = json.loads(match.group(1))
-                        targets[match.group(2)] = {
-                            "name": match.group(2),
+                        targets[match.group(1)] = {
+                            "name": match.group(1),
                             "arrived": attr}
                     else:
                         match = re_linking.search(line)
                         if match:
-                            attr = json.loads(match.group(1))
-                            attr["predicted"] = float(match.group(3))
-                            attr["error"] = float(match.group(4))
-                            attr["available"] = float(match.group(5))
-                            targets[match.group(2)]["linking"] = attr
+                            attr["predicted"] = float(match.group(2))
+                            attr["error"] = float(match.group(3))
+                            attr["available"] = float(match.group(4))
+                            targets[match.group(1)]["linking"] = attr
                         else:
                             match = re_linked.search(line)
                             if match:
-                                attr = json.loads(match.group(1))
-                                attr["predicted"] = float(match.group(3))
-                                attr["error"] = float(match.group(4))
-                                attr["actual"] = float(match.group(5))
-                                targets[match.group(2)]["linked"] = attr
+                                attr["predicted"] = float(match.group(2))
+                                attr["error"] = float(match.group(3))
+                                attr["actual"] = float(match.group(4))
+                                targets[match.group(1)]["linked"] = attr
                             else:
                                 sys.stderr.write(line)
             data.append({"jobs": jobs, "targets": [x for x in targets.values()]})
