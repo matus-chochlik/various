2a9f5eddd964213f7d1e1ce8328e2e0b2a8e998b
Matus Chochlik <chochlik@gmail.com>
2021-05-19 06:47:24 +0200

    Moved the content of the games subdir into a separate repository

--- games/openxray/fetch_cop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-steamcmd "\
-	+@sSteamCmdForcePlatformType windows" \
-	+login "${STEAMUSER:-${USER}}" \
-	+force_install_dir "$(realpath $(dirname ${0})/resources/GSC/SCOP/)" \
-	+app_update 41700 \
-	+quit
--- games/openxray/fetch_cs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-steamcmd "\
-	+@sSteamCmdForcePlatformType windows" \
-	+login "${STEAMUSER:-${USER}}" \
-	+force_install_dir "$(realpath $(dirname ${0})/resources/GSC/SCS/)" \
-	+app_update 20510 \
-	+quit
